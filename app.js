646d711267e49c768f3ab20880b008abb132a239
Priyanshu Mewada <oppriyanshu@gmail.com>
2024-07-17 19:20:40 +0530

    vercel url corrected

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ Connection();
 
 const io = new Server(PORT, {
     cors: {
-        origin: 'https://unity-pages-frontend.vercel.app/',
+        origin: 'https://unity-pages-frontend.vercel.app',
         methods: ['GET', 'POST']
     }
 });
